b90421fe5823775a81c59ae66c48b158da40a26a
khartmann5 <hartmann.kt@gmail.com>
2021-03-20 16:38:44 -0700

    saving work before tutor session

--- Mission_to_Mars/scrape.py
+++ Mission_to_Mars/scrape.py
@@ -1,7 +1,6 @@
 from splinter import Browser
 from bs4 import BeautifulSoup
 from webdriver_manager.chrome import ChromeDriverManager
-import pandas as pd
 import requests
 import pymongo
 
@@ -17,7 +16,7 @@ def scrape():
     html = browser.html
     soup = BeautifulSoup(html, "html.parser")
     mars_dict["title"] = soup.find_all("div", class_="content_title")[1].get_text()
-    mars_dict["paragraph"] = soup.find_all("div", class_="article_teaser_body")[0].get_text()
+    mars_dict["paragraph"] = soup.find("div", class_="article_teaser_body").text
 
     # Featured Mars Image
     jpl_image_url = 'https://data-class-jpl-space.s3.amazonaws.com/JPL_Space/index.html'
